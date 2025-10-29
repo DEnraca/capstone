365b9c1a4b3d09e19dc5ec8fe5a5ff509d946a53
Louiejie <louiejieg11@gmail.com>
2025-10-29 14:25:35 +0800

    Refactor queue board layout and update styles
    Improved the layout and styling of the queue board and HMO carousel for better visual consistency. Updated now-serving and queue-call Vue components for cleaner code and more consistent class usage. Replaced an HMO logo image and adjusted video and clock display for enhanced UI presentation.

--- resources/js/components/queue-call.vue
+++ resources/js/components/queue-call.vue
@@ -1,80 +1,94 @@
 <template>
+	<div
+		v-if="showModal"
+		class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+		<div
+			class="relative justify-center text-center bg-gray-100 rounded-md p-4 shadow-2xl p-10 max-w-lg w-full mx-4">
+			<audio
+				ref="modalSound"
+				:src="soundSrc"
+				autoplay
+				loop></audio>
 
-    <div v-if="showModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-    <div class="relative  justify-center text-center bg-gray-100 rounded-md p-4 shadow-2xl p-10 max-w-lg w-full mx-4">
-        <audio ref="modalSound" :src="soundSrc" autoplay loop></audio>
+			<div class="w-auto h-16 border-b">
+				<img
+					:src="logoSrc"
+					alt="Logo"
+					class="w-full h-full object-contain" />
+			</div>
 
-        <div class="w-auto h-16 border-b">
-            <img :src="logoSrc" alt="Logo" class="w-full h-full object-contain">
-        </div>
+			<div>
+				<!-- Queue number -->
+				<span
+					class="text-primary-500 font-extrabold leading-none p-2"
+					style="font-size: 3.5rem">
+					{{ queue_details.queue_number }}
+				</span>
 
-        <div>
-            <!-- Queue number -->
-            <span class="text-primary-500 font-extrabold leading-none p-2" style="font-size: 3.5rem;">
-                {{ queue_details.queue_number }}
-            </span>
-
-            <!-- Transaction type -->
-            <p class="text-primary-400 font-bold text-4xl capitalize text-gray-700 mt-6">
-                {{ queue_details.transaction }}
-            </p>
-        </div>
-    </div>
-</div>
-
+				<!-- Transaction type -->
+				<p
+					class="text-primary-400 font-bold text-4xl capitalize text-gray-700 mt-6">
+					{{ queue_details.transaction }}
+				</p>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
-   import {route} from 'ziggy-js';
-   import axios from 'axios';
-   export default {
-       data() {
-           return {
-               showModal: false,
-               queue_details: [],
-               pollInterval: null,
-               soundSrc: '/images/abr_queue_notif.wav',
-               logoSrc: '/images/logo.png',
-           };
-       },
-       methods: {
-           fetchTransactions() {
-               axios.get(route('queues-call-next'))
-                   .then(response => {
-                       if(response.data.status == 'success'){
-                        if(response.data.data.length == 0){
-                            return
-                        }
-                           this.queue_details = response.data.data;
-                           this.displayModal()
-                       }
-                   })
-                   .catch(error => {
-                       console.error("There was an error fetching the stations:", error.message);
-                   });
-           },
-           startPolling() {
-               this.showModal = false;
-               this.pollInterval = setInterval(this.fetchTransactions, 8000); // every 8 sec
-           },
-           stopPolling() {
-               if (this.pollInterval) clearInterval(this.pollInterval);
-           },
-           displayModal(){
-               this.stopPolling();
-               this.showModal = true;
-               setTimeout(() => {
-                   this.showModal = false;
-                   this.startPolling()
-               }, 5000);
-           }
-       },
+import { route } from "ziggy-js";
+import axios from "axios";
+export default {
+	data() {
+		return {
+			showModal: false,
+			queue_details: [],
+			pollInterval: null,
+			soundSrc: "/images/abr_queue_notif.wav",
+			logoSrc: "/images/logo.png",
+		};
+	},
+	methods: {
+		fetchTransactions() {
+			axios
+				.get(route("queues-call-next"))
+				.then((response) => {
+					if (response.data.status == "success") {
+						if (response.data.data.length == 0) {
+							return;
+						}
+						this.queue_details = response.data.data;
+						this.displayModal();
+					}
+				})
+				.catch((error) => {
+					console.error(
+						"There was an error fetching the stations:",
+						error.message
+					);
+				});
+		},
+		startPolling() {
+			this.showModal = false;
+			this.pollInterval = setInterval(this.fetchTransactions, 8000); // every 8 sec
+		},
+		stopPolling() {
+			if (this.pollInterval) clearInterval(this.pollInterval);
+		},
+		displayModal() {
+			this.stopPolling();
+			this.showModal = true;
+			setTimeout(() => {
+				this.showModal = false;
+				this.startPolling();
+			}, 5000);
+		},
+	},
 
-       mounted() {
-           this.startPolling();
-       },
-       beforeUnmount() {
-           this.stopPolling();
-       },
-
-   };
+	mounted() {
+		this.startPolling();
+	},
+	beforeUnmount() {
+		this.stopPolling();
+	},
+};
 </script>
