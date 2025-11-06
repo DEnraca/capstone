9cd8514c8e6c43dc1990ea277e841885dbf980ab
Louiejie <louiejieg11@gmail.com>
2025-11-06 13:29:16 +0800

    Add navigation group to DepartmentResource
    Set the navigation group to 'Maintenance' for DepartmentResource to improve sidebar organization in the Filament admin panel.

--- app/Filament/Resources/DepartmentResource.php
+++ app/Filament/Resources/DepartmentResource.php
@@ -19,6 +19,7 @@ class DepartmentResource extends Resource
 
     protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
+    protected static ?string $navigationGroup = 'Maintenance';
 
     public static function form(Form $form): Form
     {
